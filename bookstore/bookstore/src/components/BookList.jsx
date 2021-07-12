6be5cba2be6de7cc33539ec5ad2da958cda8dc81
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-07-12 01:26:40 +0100

    search

--- bookstore/bookstore/src/components/BookList.jsx
+++ bookstore/bookstore/src/components/BookList.jsx
@@ -1,7 +1,7 @@
 //1st import single book, because that is the card maker
 import SingleBook from "./SingleBook"
 //to organize the cards som cols/row are needed
-import { Col, Container, Row } from "react-bootstrap"
+import { Button, Col, Form, FormControl, Container, Row } from "react-bootstrap"
 //for doing search setState is needed, so a class component is required
 //class compoments need to import component or React
 import { Component } from "react"
@@ -9,10 +9,28 @@ import { Component } from "react"
 //booklist will receive an array of books
 class BookList extends Component {
   //creating an state where
+  state = {
+    searchQuery: "",
+  }
 
   render() {
     return (
       <Container>
+        <Row>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search Book"
+              className="mr-2"
+              aria-label="Search"
+              //binding the search with the state, saving into searchQuery
+              value={this.state.searchQuery}
+              //providing an event listener
+              onChange={(e) => this.setState({ searchQuery: e.target.value })}
+            />
+            <Button variant="secondary">Browse</Button>
+          </Form>
+        </Row>
         <Row>
           {/* inside this row multiple single cards will be displayed so 
       props.map() is needed */}
--- bookstore/bookstore/src/components/MyNav.jsx
+++ bookstore/bookstore/src/components/MyNav.jsx
@@ -1,10 +1,8 @@
-import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap"
+import { Nav, Navbar } from "react-bootstrap"
 import { Component } from "react"
 
 class MyNav extends Component {
-  state = {
-    searchQuery: "",
-  }
+ 
 
   render() {
     return (
@@ -20,19 +18,6 @@ class MyNav extends Component {
             <Nav.Link href="#action1">Home</Nav.Link>
             <Nav.Link href="#action2">About</Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search Book"
-              className="mr-2"
-              aria-label="Search"
-              //binding the search with the state, saving into searchQuery
-              value={this.state.searchQuery}
-              //providing an event listener
-              onChange={(e) => this.setState({ searchQuery: e.target.value })}
-            />
-            <Button variant="secondary">Browse</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
     )
